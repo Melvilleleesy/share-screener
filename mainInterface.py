a2fc6034e395f13dd4a61f26ee1be0f51b94b59a
Melville <melville.leesy@gmail.com>
2025-09-06 21:12:50 +0800

    Rename files and add a main entry point

---
+++ mainInterface.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+from dataCollector import get_data
+from dataScreener import screen_data
+
+load_dotenv()
+FMP_KEY = os.getenv("FMP_KEY")
+
+def main():
+    ticker = input("Please Enter a Ticker Symbol: ")
+    if isinstance(ticker, str):
+        ticker = ticker.upper()
+    company_data = get_data(ticker)
+    screen_data(company_data)
